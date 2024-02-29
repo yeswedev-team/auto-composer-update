93c6555bd10bad8be1b93164b1c9229db17a8460
Thomas <thomas.d@yeswedev.bzh>
2024-02-29 16:23:20 +0100

    feat (update filters) : update code and add filter to allow auto update of plugins and core

--- auto-composer-update.php
+++ auto-composer-update.php
@@ -3,39 +3,32 @@
 use GuzzleHttp\Client;
 
 /**
-* Plugin Name: Auto Composer Update
-* Description: A plugin that automatically updates the composer when a WordPress plugin is updated.
-* Version: 3.0.1
-* Author: Yes We Dev
-* Author URI: https://yeswedev.bzh/
-*/
+ * Plugin Name: Auto Composer Update
+ * Description: A plugin that automatically updates the composer when a WordPress plugin is updated.
+ * Version: 3.0.1
+ * Author: Yes We Dev
+ * Author URI: https://yeswedev.bzh/
+ */
 
-function write_log($log) {
-    if ( ! function_exists('write_log')) {
-        function write_log ( $log )  {
-            if ( is_array( $log ) || is_object( $log ) ) {
-                error_log( print_r( $log, true ) );
-            } else {
-                error_log( $log );
-            }
-        }
-    }
-}
-
-function get_composer_json(string $path) {
+function get_composer_json(string $path): mixed
+{
     $composer_json_path = $path . '/composer.json';
 
     $composer_json = file_get_contents($composer_json_path);
+
     if (!$composer_json) {
-        write_log('Can\'t read composer.json.');
-        return;
+        error_log('Error : Can\'t read composer.json.');
+
+        return false;
     }
-    
+
     return json_decode($composer_json, true) ?: false;
 }
 
-function update_composer(array $body) {
+function update_composer(array $body): bool
+{
     $client = new Client();
+
     try {
         $response = $client->post(
             env('API_UPDATE_WORDPRESS'),
@@ -43,59 +36,84 @@ function update_composer(array $body) {
         );
 
         if ($response->getStatusCode() === 500) {
-            write_log('Error: ' . $response->getBody());
+            error_log('Error : The WordPress and plugins update on the remote repository (' . env('GIT_BRANCH') . ') has failed .');
+            error_log('Error: ' . $response->getBody());
+
             return false;
         }
-        
+
+        error_log('Info : The WordPress and plugins update on the remote repository (' . env('GIT_BRANCH') . ') was successful.');
+
         return true;
-    } catch (\GuzzleHttp\Exception\GuzzleException $e) {
-        write_log($e);
+    } catch (\GuzzleHttp\Exception\GuzzleException $exception) {
+        error_log('Error : The WordPress and plugins update on the remote repository (' . env('GIT_BRANCH') . ') has failed .');
+        error_log('Error : ' . print_r($exception, true));
+
         return false;
     }
 }
 
-function get_latest_wp_version() {
-    global $wp_version;
-    $updates = get_core_updates();
-
-    if (!is_array($updates)) {
-        write_log('Unable to retrieve update information.');
-        return $wp_version;
-    } else {
-        $latest_update = array_shift($updates);
-        return $latest_update->current;
+function on_upgrader_process_complete(array $update_results): void
+{
+    if (empty($update_results)) {
+        return;
     }
-}
 
-function on_upgrader_process_complete(array $update_results) {
     $body = [
         'git' => env('GIT_REPOSITORY'),
         'branch' => env('GIT_BRANCH'),
-        'wordpressVersion' => get_latest_wp_version(),
+        'wordpressVersion' => get_bloginfo('version'),
         'plugins' => []
     ];
 
-    $composer_json = get_composer_json(env('WP_CURRENT_PATH'));
+    /**
+     * @var string $key
+     * @var array $value
+     */
+    foreach ($update_results as $key => $value) {
+        switch ($key) {
+            case 'core':
+                if (empty($value)) {
+                    error_log('Error : No WordPress update has been found.');
 
-    foreach ($update_results['plugin'] as $plugin) {
-        foreach ($composer_json['require'] as $name => $version) {
-            $parts = explode('/', $name);
-            $packageSlug = end($parts);
-            if ($packageSlug === $plugin->item->slug) {
-                $body['plugins'][] = [
-                    'name' => $name,
-                    'version' => $plugin->item->new_version
-                ];
-            }
+                    break;
+                }
+
+                /** @var string $version */
+                $version = $value[0]->item->version;
+                $body['wordpressVersion'] = $version;
+
+                error_log('Info : A new version of WordPress has been installed locally (' . $version . ').');
+                break;
+            case 'plugin':
+                $composer_json = get_composer_json(env('WP_CURRENT_PATH'));
+
+                foreach ($value as $plugin) {
+                    foreach ($composer_json['require'] as $name => $version) {
+                        $parts = explode('/', $name);
+                        $packageSlug = end($parts);
+                        if ($packageSlug === $plugin->item->slug) {
+                            $body['plugins'][] = [
+                                'name' => $name,
+                                'version' => $plugin->item->new_version
+                            ];
+
+                            error_log('Info : A new update of ' . $name . ' has been installed locally (' . $plugin->item->new_version . ').');
+                        }
+                    }
+                }
+                break;
         }
     }
 
     $update_result = update_composer($body);
 
     if (!$update_result) {
-        write_log('Error while updating composer.json');
+        error_log('Error while updating composer.json');
     }
 }
 
-add_filter( 'automatic_updates_is_vcs_checkout', '__return_false', 1 );
+add_filter('automatic_updates_is_vcs_checkout', '__return_false', 1);
+add_filter('auto_update_plugin', '__return_true', 1);
+add_filter('auto_update_core', '__return_true', 1);
 add_action('automatic_updates_complete', 'on_upgrader_process_complete', 10, 2);
