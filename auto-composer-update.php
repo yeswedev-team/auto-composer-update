be7f909bf15dfd68623b31af0ee74f6223ffdc31
Thomas <thomas.d@yeswedev.bzh>
2024-03-04 11:23:58 +0100

    feat : add UPDATE_REPOSITORY true or false

--- auto-composer-update.php
+++ auto-composer-update.php
@@ -5,7 +5,7 @@ use GuzzleHttp\Client;
 /**
  * Plugin Name: Auto Composer Update
  * Description: A plugin that automatically updates the composer when a WordPress plugin is updated.
- * Version: 4.2.1
+ * Version: 4.3.0
  * Author: Yes We Dev
  * Author URI: https://yeswedev.bzh/
  */
